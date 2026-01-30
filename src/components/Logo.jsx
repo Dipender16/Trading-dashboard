d91bbbecb1b0ec6597f5634c7ca6ca2b874da810
Dipender shakya <dipendershakya@gmail.com>
2026-01-31 03:00:35 +0530

    logo issue solving

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
 function Logo({
-  src= "https://app.tradezella.com/static/media/tradezella-logo.db71bfd13dce99dd3e33.png",
+  src= "/LOGO.png",
   className
 }) {
   return (
-    <img className={`w-8 md:w-12 ${className}`} src={src} alt="" />
+    <img className={`w-24 md:w-36 ${className}`} src={src} alt="" />
   )
 }
 
